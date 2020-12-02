869c9670abbc00af14ca7efa8eef3d58618496a7
hhh2012aa <38385178+hhh2012aa@users.noreply.github.com>
2020-12-02 16:45:20 +0800

    init

---
+++ create-configute.py
@@ -0,0 +1,15 @@
+import fileinput
+
+s3_replacement_text = input("s3 name: ")
+elb_replacement_text = input("elb domain name: ")
+# Read in the file
+with open('distribution-config copy.json', 'r') as file :
+  filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace('<s3-name>', s3_replacement_text)
+filedata = filedata.replace('<wp-alb-domain>', elb_replacement_text)
+
+# Write the file out again
+with open('new.json', 'w') as file:
+  file.write(filedata)
